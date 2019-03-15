11886173751f0137eef317636fcf72fa883bec4d
Eduardo Sotero <eduardo@feracode.com>
2019-03-15 18:32:05 -0300

    Updates

---
+++ precache-manifest.8de1f74f0499b87a05ac1fb3afa8ee3f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "5e22c14add05f750cba3",
+    "url": "/context-store/static/js/runtime~main.b7daaf50.js"
+  },
+  {
+    "revision": "e33caea640c7c043149e",
+    "url": "/context-store/static/js/main.73aa92d4.chunk.js"
+  },
+  {
+    "revision": "6e39ec2297305b9b0834",
+    "url": "/context-store/static/js/2.5d381ac9.chunk.js"
+  },
+  {
+    "revision": "e230318c22733edf9231227ca12a200a",
+    "url": "/context-store/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.02fa10f6618027a70b5dfd589ef8343f.js"
+  "/context-store/precache-manifest.8de1f74f0499b87a05ac1fb3afa8ee3f.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/context-store/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
