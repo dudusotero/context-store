0ddafb54c16fc21ebffbd9b54e1f17a0adc62df6
Eduardo Sotero <eduardo@feracode.com>
2019-03-15 18:27:37 -0300

    remove some logs

--- src/components/UserInfo/reducer.js
+++ src/components/UserInfo/reducer.js
@@ -6,10 +6,8 @@ export const initialState = {
 export default (state, action) => {
   switch (action.type) {
     case 'USER_FETCH':
-      console.log('fetchData')
       return { ...state, loading: true }
     case 'USER_FETCH_SUCCESS':
-      console.log('fetchSuccess')
       return { loading: false, data: action.payload }
     case 'USER_FETCH_ERROR':
       return { loading: false }
